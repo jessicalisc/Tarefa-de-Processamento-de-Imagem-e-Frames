a17608e71f3689d1324f066669ad3589ea8896f3
Jessica Ligia <jessicalisc@hotmail.com>
2019-11-18 19:34:35 -0300

    Atualizações, realizando alterações e comentários ao código.

--- Video.py
+++ Video.py
@@ -6,16 +6,15 @@ cap = cv2.VideoCapture('Tom e Jerry.avi')
 
 i = 1
 while(cap.isOpened()):
-    ret, frame = cap.read() #fazer tramento do ultimo while
+    ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('Frame em Cinza',gray)
+    cv2.imshow('Frame em Cinza',gray) # Este filtro muda a coloraçao do frame selecionado para cinza.
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 100): #1822 frames
+    if (i == 100):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
@@ -28,7 +27,6 @@ while(cap.isOpened()):
         break
     if (i == 200):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
@@ -41,158 +39,133 @@ while(cap.isOpened()):
         break
     if (i == 300):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
-    ret, frame = cap.read() #fazer tramento do ultimo while
+    ret, frame = cap.read() 
     (h, w, d) = frame.shape
     r = 300.0 / w
     dim = (300, int(h * r))
     proporcao = cv2.resize(frame, dim)
-    cv2.imshow('Redimensionar por proporcao',proporcao)
+    cv2.imshow('Redimensionar por proporcao',proporcao) #
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
-    if (i == 400): #1822 frames
+    if (i == 400):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
     ret, frame = cap.read()
-    redimutils = imutils.resize(frame, width=300)
+    redimutils = imutils.resize(frame, width=300) # este filtro redimensiona a frame conforme o tamanho especificado
 
-    cv2.imshow('Redimensionar pelo Imutils',redimutils)
+    cv2.imshow('Redimensionar pelo Imutils',redimutils)# apresenta em tela os resultados do filtro aplicado.
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 500): #1822 frames
+    if (i == 500):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     center = (w // 2, h // 2)
     M = cv2.getRotationMatrix2D(center, -180, 1.0)
-    rotacao = cv2.warpAffine(frame, M, (w, h))
+    rotacao = cv2.warpAffine(frame, M, (w, h)) #este filtro esta fazendo a rotaçao da frame apartir das dimensoes especificadas.
 
-    cv2.imshow('Rotacao',rotacao)
+    cv2.imshow('Rotacao OpenCV',rotacao) # apresenta em tela o resultado do filtro de rotaçao
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 600): #1822 frames
+    if (i == 600):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
-    ret, frame = cap.read() #fazer tramento do ultimo while
+    ret, frame = cap.read()
 
-    rotacaoim = imutils.rotate(frame, -45) # rotacao da imagem pela biblioteca imutils
+    rotacaoim = imutils.rotate(frame, -45) # rotacao da imagem pela biblioteca imutils em 45 graus
 
-    cv2.imshow('Rotacao Imutils',rotacaoim)
+    cv2.imshow('Rotacao Imutils',rotacaoim) #apresenta em tela o resultado do filtro.
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 700): #1822 frames
+    if (i == 700):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
-    ret, frame = cap.read() #fazer tramento do ultimo while
+    ret, frame = cap.read()
 
-    rota = imutils.rotate_bound(frame, 90)
+    rota = imutils.rotate_bound(frame, 90) #filtro responsavel por mudar a posiçao da frame em 90 graus
 
-    cv2.imshow('Rotacao Modificada',rota)
+    cv2.imshow('Rotacao Modificada',rota) # apresenta em video o resultado do filtro
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 800): #1822 frames
+    if (i == 800):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
-    ret, frame = cap.read() #fazer tramento do ultimo while
+    ret, frame = cap.read()
 
-    rota = imutils.rotate_bound(frame, 60)
+    desfoque = cv2.GaussianBlur(frame, (11, 11), 0) #filtro responsavel por desfocar a imagem toda das frames.
 
-    cv2.imshow('Rotacao Modificada',rota)
+    cv2.imshow('Desfocar o Video ',desfoque) #apresenta no video os resultados do filtro
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 800): #1822 frames
+    if (i == 900):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
-    ret, frame = cap.read() #fazer tramento do ultimo while
-
-    desfoque = cv2.GaussianBlur(frame, (11, 11), 0)
-
-    cv2.imshow('Desfocar o Video ',desfoque)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-    if (i == 900): #1822 frames
-        break
-    #print (i)
-    i +=1
-
-while(cap.isOpened()):
-    ret, frame = cap.read() #fazer tramento do ultimo while
+    ret, frame = cap.read()
 
     output = frame.copy()
-    #apresenta um retangulo sobre a imagem
-    cv2.rectangle(output, (380, 360), (250, 100), (0, 0, 255), 5)
+    cv2.rectangle(output, (380, 360), (250, 100), (0, 0, 255), 5)# filtro responsavel por modelar um retandulo coma as seguintes especificações sobre as frames.
 
-    cv2.imshow('Retangulo sobre o Video',output)
+    cv2.imshow('Retangulo sobre o Video',output) # responsavel apresentar em tela um retangulo sobre o video.
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 1000): #1822 frames
+    if (i == 1000):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
     ret, frame = cap.read()
 
     saida = frame.copy()
-    cv2.circle(saida, (150, 50), 40, (255, 10, 0), -9)
+    cv2.circle(saida, (150, 50), 40, (255, 10, 0), -9) # filtro que medela um circulo a partir das dimensões aplicadas aqui.
 
-    cv2.imshow('Circulo sobre o Video',saida)
+    cv2.imshow('Circulo sobre o Video',saida) #responsavel por apresentar na tela um circulo sobre o video.
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 1100): #1822 frames
+    if (i == 1100):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
-    ret, frame = cap.read() #fazer tramento do ultimo while
+    ret, frame = cap.read()
 
     saida = frame.copy()
-    cv2.line(saida, (160, 20), (250, 200), (0, 0, 255), 5)
+    cv2.line(saida, (160, 20), (250, 200), (0, 0, 255), 5) #filtro que modela uma linha em determinados frames e posições na matriz do video.
 
-    cv2.imshow('Linha sobre o Video',saida)
+    cv2.imshow('Linha sobre o Video',saida)# apresenta sobre o video o objeto aplicado no filtro no caso uma linha na diagonal vermelha.
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 1200): #1822 frames
+    if (i == 1200):
         break
-    #print (i)
     i +=1
 
 while(cap.isOpened()):
-    ret, frame = cap.read() #fazer tramento do ultimo while
+    ret, frame = cap.read()
 
     saida = frame.copy()
-    cv2.putText(saida, "OpenCV + Tom e Jerry!!!", (20, 20),
+    cv2.putText(saida, "OpenCV + Tom e Jerry!!!", (20, 20),# filtro responsavel por colocar uma simples frase em determinados frames e posiçoes na matriz
         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
-    cv2.imshow('Frase aplicada sobre o Video',saida)
+    cv2.imshow('Frase aplicada sobre o Video',saida)# apresentação em tela do trabalho realizado pelo filtro, no caso a frase"Opencv + Tom e Jerry"
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 1300): #1822 frames
+    if (i == 1300):
         break
-    #print (i)
     i +=1
 
 cap.release()
