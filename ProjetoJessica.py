09fb31958a9dc2ac5913d6e3970fd538a57711e2
Jessica Ligia <jessicalisc@hotmail.com>
2019-11-28 13:51:35 -0300

    Finalização do Trabalho.

--- ProjetoJessica.py
+++ ProjetoJessica.py
@@ -1,106 +1,96 @@
 # USAGE
 # python opencv_tutorial_01.py
 
-# import the necessary packages
+# importando as bibliotecas necessárias
 import imutils
 import cv2
 
-# load the input image and show its dimensions, keeping in mind that
-# images are represented as a multi-dimensional NumPy array with
-# shape no. rows (height) x no. columns (width) x no. channels (depth)
+# carregando imagem
 image = cv2.imread("jerry.jpg")
 (h, w, d) = image.shape
 print("width={}, height={}, depth={}".format(w, h, d))
 
-# display the image to our screen -- we will need to click the window
-# open by OpenCV and press a key on our keyboard to continue execution
-#apresentacao da imagem selecionada
+# Para apresentar a imagem modificada precisamos apertar uma tecla para passar para a proxima imagem
 cv2.imshow("Imagem", image)
 cv2.waitKey(0)
 
-# access the RGB pixel located at x=50, y=100, keepind in mind that
-# OpenCV stores images in BGR order rather than RGB
+# Salvando imagem em BGR por causa do OpenCV que não salva em RGB
 (B, G, R) = image[100, 50]
 print("R={}, G={}, B={}".format(R, G, B))
 
-# extract a 100x100 pixel square ROI (Region of Interest) from the
-# input image starting at x=320,y=60 at ending at x=420,y=160
+
 #corta a imagem nos parametro selecionados
 roi = image[60:160, 200:310]
-cv2.imshow("ROI", roi)
+cv2.imshow("Cortando Imagem", roi)
 cv2.waitKey(0)
 
-# resize the image to 200x200px, ignoring aspect ratio
+# no nosso caso as dimenções são 80x100px
 #diminui a imagem para as dimensoes especificadas abaixo
 resized = cv2.resize(image, (80, 100))
 cv2.imshow("Redimensionamento Fixo", resized)
 cv2.waitKey(0)
 
-# fixed resizing and distort aspect ratio so let's resize the width
-# to be 300px but compute the new height based on the aspect ratio
+# calculo manual para ver a proporção da imagem e apresenta-la
 r = 300.0 / w
 dim = (300, int(h * r))
 resized = cv2.resize(image, dim)
 cv2.imshow("Redimensionamento pela Proporcao", resized)
 cv2.waitKey(0)
 
-# manually computing the aspect ratio can be a pain so let's use the
-# imutils library instead
+# usar a biblioteca Imutils para calcular a proporção
 resized = imutils.resize(image, width=300)
 cv2.imshow("Redimensionar Imutils", resized)
 cv2.waitKey(0)
 
-# let's rotate an image 45 degrees clockwise using OpenCV by first
-# computing the image center, then constructing the rotation matrix,
-# and then finally applying the affine warp
+# a imagem sera girada em 45 graus no sentido horario pelo Opencv
+# E depois aplicar a distorção
 center = (w // 2, h // 2)
 M = cv2.getRotationMatrix2D(center, -45, 1.0)
 rotated = cv2.warpAffine(image, M, (w, h))
 cv2.imshow("Rotacao OpenCV", rotated)
 cv2.waitKey(0)
 
-# rotation can also be easily accomplished via imutils with less code
-rotated = imutils.rotate(image, -45)
+# aqui é possivel ver uma rotação por meio do Imutils que tem o cuidado de nao cortar a imagem
+rotated = imutils.rotate(image, -180)
 cv2.imshow("Rotacao Imutils", rotated)
 cv2.waitKey(0)
 
-# OpenCV doesn't "care" if our rotated image is clipped after rotation
-# so we can instead use another imutils convenience function to help
-# us out
-rotated = imutils.rotate_bound(image, 45)
+# O OpenCV possivelmente cortará a imagem na rotação, mas aqui usaremos como auxilio o imutils
+# pois ele não deixa a imagem ser cortada.
+rotated = imutils.rotate_bound(image, 90)
 cv2.imshow("Imutils Bound Rotation", rotated)
 cv2.waitKey(0)
 
-# apply a Gaussian blur with a 11x11 kernel to the image to smooth it,
-# useful when reducing high frequency noise
+# aplica o filtro gaussiano em 11x11 para suavizar a imagem
+# reduz ruídos de alta frequência
 #embaça a imagem
 blurred = cv2.GaussianBlur(image, (11, 11), 0)
 cv2.imshow("Desfocar", blurred)
 cv2.waitKey(0)
 
-# draw a 2px thick red rectangle surrounding the face
+# desenha um retângulo vermelho sobre a imagem
 output = image.copy()
 #apresenta um retangulo sobre a imagem
 cv2.rectangle(output, (180, 360), (200, 160), (0, 0, 255), 2)
-cv2.imshow("Retangulo", output)
+cv2.imshow("Retangulo Vermelho", output)
 cv2.waitKey(0)
 
-# draw a blue 20px (filled in) circle on the image centered at
-# x=300,y=150
+# desenhe um circulo azul 20px (de preenchimento) nas seguintes posições
+# x=150,y=150
 #apresenta um circulo sobre a imagem
 output = image.copy()
 cv2.circle(output, (150, 150), 20, (255, 0, 0), -1)
-cv2.imshow("Circulo", output)
+cv2.imshow("Circulo Azul", output)
 cv2.waitKey(0)
 
-# draw a 5px thick red line from x=60,y=20 to x=400,y=200
+# desenhe uma linha vermelha com as proporções x=60,y=20 por x=200,y=200
 #apresenta uma linha sobre a imagem
 output = image.copy()
 cv2.line(output, (60, 20), (200, 200), (0, 0, 255), 5)
-cv2.imshow("Linha", output)
+cv2.imshow("Linha Vermelha", output)
 cv2.waitKey(0)
 
-# draw green text on the image
+
 #apresenta um texto sobre a imagem
 output = image.copy()
 cv2.putText(output, "OpenCV + Jerry!!!", (10, 20),
--- Video.py
+++ Video.py
@@ -1,26 +1,33 @@
+# importando bibliotecas necessárias
 import numpy as np
 import cv2
 import imutils
 
+# carregando vídeo
 cap = cv2.VideoCapture('Tom e Jerry.avi')
 
-i = 1
+i = 1 #capturando primeira frame
+
+#Este primeiro filtro tem a capacidade de alterar a coloração
+# do frame para cinza
 while(cap.isOpened()):
     ret, frame = cap.read()
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('Frame em Cinza',gray) # Este filtro muda a coloraçao do frame selecionado para cinza.
+    cv2.imshow('Frame em Cinza',gray)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
     if (i == 100):
         break
     i +=1
 
+# Este filtro cora o frame em pixes especificos no caso
+# frames x=90:350 e y=50:310
 while(cap.isOpened()):
     ret, frame = cap.read()
 
-    cortar = frame[90:350, 50:310] #corta o video em pixes especificos
+    cortar = frame[90:350, 50:310]
 
     cv2.imshow('Frame recortada',cortar)
     if cv2.waitKey(20) & 0xFF == ord('q'):
@@ -29,6 +36,8 @@ while(cap.isOpened()):
         break
     i +=1
 
+# Redimensionar nada mais é que diminuir a frame nos valores descrito aqui
+# no caso x= 160 e y=300
 while(cap.isOpened()):
     ret, frame = cap.read()
 
@@ -41,13 +50,17 @@ while(cap.isOpened()):
         break
     i +=1
 
+
+# O redimensionamento por proporção nada mais é do que
+# diminuir a frame nas proporções indicadas, porem fazer ajustes
+# necessários para que a imagem do frame ao final nao saia distorcida como anteriormente
 while(cap.isOpened()):
-    ret, frame = cap.read() 
+    ret, frame = cap.read()
     (h, w, d) = frame.shape
     r = 300.0 / w
     dim = (300, int(h * r))
     proporcao = cv2.resize(frame, dim)
-    cv2.imshow('Redimensionar por proporcao',proporcao) #
+    cv2.imshow('Redimensionar por proporcao',proporcao)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
@@ -55,54 +68,61 @@ while(cap.isOpened()):
         break
     i +=1
 
+# Este filtro vai fazer o mesmo trabalho do anterio com a cautela de não cortar a frame
+# por causa da biblioteca Imutils
 while(cap.isOpened()):
     ret, frame = cap.read()
-    redimutils = imutils.resize(frame, width=300) # este filtro redimensiona a frame conforme o tamanho especificado
+    redimutils = imutils.resize(frame, width=300)
 
-    cv2.imshow('Redimensionar pelo Imutils',redimutils)# apresenta em tela os resultados do filtro aplicado.
+    cv2.imshow('Redimensionar pelo Imutils',redimutils)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
     if (i == 500):
         break
     i +=1
 
+# A rotação pelo OpenCV faz a modificação da frame em suas proporções normais
+#
 while(cap.isOpened()):
     ret, frame = cap.read()
     center = (w // 2, h // 2)
     M = cv2.getRotationMatrix2D(center, -180, 1.0)
-    rotacao = cv2.warpAffine(frame, M, (w, h)) #este filtro esta fazendo a rotaçao da frame apartir das dimensoes especificadas.
+    rotacao = cv2.warpAffine(frame, M, (w, h))
 
-    cv2.imshow('Rotacao OpenCV',rotacao) # apresenta em tela o resultado do filtro de rotaçao
+    cv2.imshow('Rotacao OpenCV',rotacao)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
     if (i == 600):
         break
     i +=1
 
+# Este filtro busca fazer as transformadas na frame com o cuidado de não corta as imagens
 while(cap.isOpened()):
     ret, frame = cap.read()
 
-    rotacaoim = imutils.rotate(frame, -45) # rotacao da imagem pela biblioteca imutils em 45 graus
+    rotacaoim = imutils.rotate(frame, -15)
 
-    cv2.imshow('Rotacao Imutils',rotacaoim) #apresenta em tela o resultado do filtro.
+    cv2.imshow('Rotacao Imutils',rotacaoim)
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
     if (i == 700):
         break
     i +=1
 
+# Filtro de Rotação neste caso responsavel por fazer uma rotação de 90 graus na frame
 while(cap.isOpened()):
     ret, frame = cap.read()
 
-    rota = imutils.rotate_bound(frame, 90) #filtro responsavel por mudar a posiçao da frame em 90 graus
+    rota = imutils.rotate_bound(frame, 90)
 
     cv2.imshow('Rotacao Modificada',rota) # apresenta em video o resultado do filtro
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
-    if (i == 800):
+    if (i == 800): #para na frame 800
         break
     i +=1
 
+# Neste filtro é aplicado o filtro de gaussiano com proporções de 11x11 desfocando a frame por inteira
 while(cap.isOpened()):
     ret, frame = cap.read()
 
@@ -115,11 +135,14 @@ while(cap.isOpened()):
         break
     i +=1
 
+
+# Filtro responsavel por apresentar um retângulo vermelho na tela da frames
+#nas proporções especificadas abaixo:
 while(cap.isOpened()):
     ret, frame = cap.read()
 
     output = frame.copy()
-    cv2.rectangle(output, (380, 360), (250, 100), (0, 0, 255), 5)# filtro responsavel por modelar um retandulo coma as seguintes especificações sobre as frames.
+    cv2.rectangle(output, (380, 360), (250, 100), (0, 0, 255), 5)
 
     cv2.imshow('Retangulo sobre o Video',output) # responsavel apresentar em tela um retangulo sobre o video.
     if cv2.waitKey(20) & 0xFF == ord('q'):
@@ -128,6 +151,8 @@ while(cap.isOpened()):
         break
     i +=1
 
+# o Circulo sobre o vídeo nada mais é do que um filtro que aplica em determinada posição da frame
+# um circulo no nosso caso azul
 while(cap.isOpened()):
     ret, frame = cap.read()
 
@@ -141,6 +166,8 @@ while(cap.isOpened()):
         break
     i +=1
 
+
+# Neste filtro é desenhado uma linha vermelha sobre as frames nas posições especificadas abaixo
 while(cap.isOpened()):
     ret, frame = cap.read()
 
@@ -154,11 +181,13 @@ while(cap.isOpened()):
         break
     i +=1
 
+
+#filtro responsavel por aplicar um frase de sua escolha sobre as frames aqui apresentadas
 while(cap.isOpened()):
     ret, frame = cap.read()
 
     saida = frame.copy()
-    cv2.putText(saida, "OpenCV + Tom e Jerry!!!", (20, 20),# filtro responsavel por colocar uma simples frase em determinados frames e posiçoes na matriz
+    cv2.putText(saida, "OpenCV + Tom e Jerry = Sucesso!!!", (20, 20),# filtro responsavel por colocar uma simples frase em determinados frames e posiçoes na matriz
         cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
 
     cv2.imshow('Frase aplicada sobre o Video',saida)# apresentação em tela do trabalho realizado pelo filtro, no caso a frase"Opencv + Tom e Jerry"
